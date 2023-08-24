f17a71a8caa981ac5a0f15f9557aa1bedf1f4846
giorgos papageorgiou <gpap.gpap@gmail.com>
2023-08-24 11:40:41 +0100

    test dependency

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import subprocess
+import sys
+import os
+import time
+
+try:
+    # replace "yourpackage" with the package you want to import
+    import mantis
+
+# This block executes only on the first run when your package isn't installed
+except ModuleNotFoundError as e:
+    st.write(f"worked to module not found {os.environ['git_token']}")
+    subprocess.Popen(
+        [
+            f"{sys.executable} -m pip install git+https://${os.environ['git_token']}@github.com/gpap-gpap/anisotroPY.git@dev-fAVO"
+        ],
+        shell=True,
+    )
+    # wait for subprocess to install package before running your actual code below
+    time.sleep(90)
 from mantis_vision.look_and_feel import *
 from mantis_vision.showcase_loading import *
 
