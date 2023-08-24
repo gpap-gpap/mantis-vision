e4ee2dc5d5bd9f96524f603387d87654216cadbf
giorgos papageorgiou <gpap.gpap@gmail.com>
2023-08-24 11:59:58 +0100

    try again

--- main.py
+++ main.py
@@ -11,15 +11,19 @@ try:
 
 # This block executes only on the first run when your package isn't installed
 except ModuleNotFoundError as e:
-    st.write(f"worked to module not found {st.secrets['git_token']}")
+    sleep_time = 90
+    dependency_warning = st.warning(
+        f"Installing dependencies, this takes {sleep_time} seconds."
+    )
     subprocess.Popen(
         [
-            f"{sys.executable} -m pip install git+https://${st.secrets['git_token']}@github.com/gpap-gpap/anisotroPY.git@dev-fAVO"
+            f"{sys.executable} -m pip install git+https://${st.secrets['git_token']}@github.com/gpap-gpap/anisotroPY.git"
         ],
         shell=True,
     )
     # wait for subprocess to install package before running your actual code below
-    time.sleep(90)
+    time.sleep(sleep_time)
+    dependency_warning.empty()
 from mantis_vision.look_and_feel import *
 from mantis_vision.showcase_loading import *
 
